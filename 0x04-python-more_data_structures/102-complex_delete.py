58be7df68d9c8bee7728bf26c232d9af108c3d1b
Charles <charlesmisheal@gmail.com>
2022-09-29 12:54:06 -0700

    task102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(my_dict, value):
+        if my_dict[key] == value:
+            keys_to_del.append(key)
+    for key in keys_to_del:
+        del my_dict[key]
+    return my_dict
